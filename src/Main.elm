0eb3f5b37e13d6675675201e34c78d21426fd40c
Henrique Gasques <h.gasques@paywith.com>
2022-05-11 01:58:46 -0300

    Check guess and color letter backgroud accordingly

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, input, text)
+import Html exposing (Html, button, div, input, span, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
 
@@ -12,7 +12,7 @@ main =
 
 
 type alias Model =
-    { wordle : String
+    { wordle : List String
     , guesses : List String
     , currentGuess : String
     , maxAttempts : Int
@@ -21,7 +21,7 @@ type alias Model =
 
 init : Model
 init =
-    { wordle = "HELLO", guesses = [], currentGuess = "", maxAttempts = 5 }
+    { wordle = [ "H", "E", "L", "L", "O" ], guesses = [], currentGuess = "", maxAttempts = 5 }
 
 
 type Msg
@@ -34,7 +34,7 @@ update msg model =
     case msg of
         Check guess ->
             { model
-                | guesses = List.append model.guesses [ guess ]
+                | guesses = guess :: model.guesses
                 , currentGuess = ""
             }
 
@@ -43,14 +43,18 @@ update msg model =
                 model
 
             else
-                { model | currentGuess = currentGuess }
+                { model | currentGuess = String.toUpper currentGuess }
 
 
 view : Model -> Html Msg
 view model =
     div []
-        ([ div [] (List.map (\guess -> div [] [ text guess ]) model.guesses) ]
-            ++ (if List.length model.guesses < model.maxAttempts then
+        (div []
+            (List.map
+                (\guess -> div [ style "margin" "12px" ] (checkGuess guess model.wordle))
+                model.guesses
+            )
+            :: (if List.length model.guesses < model.maxAttempts then
                     [ div []
                         [ input [ value model.currentGuess, onInput Change ] []
                         , button [ onClick (Check model.currentGuess) ] [ text "Check" ]
@@ -61,3 +65,33 @@ view model =
                     []
                )
         )
+
+
+checkGuess : String -> List String -> List (Html msg)
+checkGuess guess wordle =
+    let
+        defaulStyle =
+            [ style "padding" "5px"
+            , style "margin" "2px"
+            , style "color" "white"
+            ]
+    in
+    List.map2
+        (\guessChar wordleChar ->
+            if guessChar == wordleChar then
+                span
+                    (style "background-color" "MediumSeaGreen" :: defaulStyle)
+                    [ text guessChar ]
+
+            else if List.member guessChar wordle then
+                span
+                    (style "background-color" "GoldenRod" :: defaulStyle)
+                    [ text guessChar ]
+
+            else
+                span
+                    (style "background-color" "gray" :: defaulStyle)
+                    [ text guessChar ]
+        )
+        (String.split "" guess)
+        wordle
